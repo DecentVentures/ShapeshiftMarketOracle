61876fadb2c27235826855e085e321da933d601c
Micah Riggan <micahriggan@gmail.com>
2017-12-29 16:20:18 -0600

    Updating holding contract with fallback to accept deposits

--- contracts/ShapeshiftHolding.sol
+++ contracts/ShapeshiftHolding.sol
@@ -2,47 +2,52 @@ pragma solidity ^0.4.17;
 import "./ShapeshiftOracle.sol";
 
 contract ShapeshiftHolding {
-	ShapeshiftOracle marketOracle;
-	uint constant BILLION = 1000000000;
+  ShapeshiftOracle marketOracle;
+  uint constant BILLION = 1000000000;
 
-	bytes32 public _pair;
-	address public _shapeshiftAddr;
-	address public _owner;
+  bytes32 public _pair;
+  address public _shapeshiftAddr;
+  address public _owner;
+  event Deposit(uint amount, address sender);
 
-	function ShapeshiftHolding(address oracle, bytes32 pairName, address shiftAddr) public {
-		_owner = msg.sender;
-		_pair = pairName;
-		_shapeshiftAddr = shiftAddr;
-		marketOracle = ShapeshiftOracle(oracle);
-	}
+  function ShapeshiftHolding(address oracle, bytes32 pairName, address shiftAddr) public {
+    _owner = msg.sender;
+    _pair = pairName;
+    _shapeshiftAddr = shiftAddr;
+    marketOracle = ShapeshiftOracle(oracle);
+  }
 
-	modifier isOwner() {
-		require(_owner == msg.sender);
-		_;
-	}
-	modifier canSend() {
-		var (pair, ratePPB, limitPPB, minPPB, minerFeePPB, active) = marketOracle.markets(_pair);
-		require(pair != 0);
-		require(active);
-		require(this.balance / BILLION  > minPPB);
-		require(this.balance / BILLION  < limitPPB);
-		_;
-	}
+  modifier isOwner() {
+    require(_owner == msg.sender);
+    _;
+  }
+  modifier canSend() {
+    var (pair, ratePPB, limitPPB, minPPB, minerFeePPB, active) = marketOracle.markets(_pair);
+    require(pair != 0);
+    require(active);
+    require(this.balance / BILLION  > minPPB);
+    require(this.balance / BILLION  < limitPPB);
+    _;
+  }
 
-	function toShapeshift() canSend public{
-		var (,,limit,,,) = marketOracle.markets(_pair);
-		if(this.balance / BILLION > limit) {
-			_shapeshiftAddr.transfer(limit / BILLION);
-		}	else {
-			_shapeshiftAddr.transfer(this.balance);
-		}
-	}
+  function toShapeshift() canSend public{
+    var (,,limit,,,) = marketOracle.markets(_pair);
+    if(this.balance / BILLION > limit) {
+      _shapeshiftAddr.transfer(limit / BILLION);
+    } else {
+      _shapeshiftAddr.transfer(this.balance);
+    }
+  }
 
-	function widthdraw(address to, uint amount) isOwner public {
-		to.transfer(amount);
-	}
+  function () public payable {
+    Deposit(msg.value, msg.sender);
+  }
 
-	function widthdrawAll(address to) isOwner public {
-		to.transfer(this.balance);
-	}
+  function widthdraw(address to, uint amount) isOwner public {
+    to.transfer(amount);
+  }
+
+  function widthdrawAll(address to) isOwner public {
+    to.transfer(this.balance);
+  }
 }
